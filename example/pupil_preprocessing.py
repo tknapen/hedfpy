1506e6e7c189fa789325f72980b2430d88f9bf1b
Jan Willem de Gee <jwdegee@gmail.com>
2017-07-29 15:55:25 +0200

    cleanup

---
+++ example/pupil_preprocessing.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os, glob
+from IPython import embed as shell
+
+import hedfpy
+
+task = 'yesno'
+raw_dir = 'raw/'
+output_dir = os.path.expanduser('~/Downloads/pupil_prep/')
+subjects = [
+            'sub-01',
+            # 'sub-02',
+            ]
+
+def preprocess_subjects(subjects):
+    
+    for subject in subjects:
+    
+        # data:
+        edfs = glob.glob(os.path.join(raw_dir, '{}*{}*.edf'.format(subject, task)))
+    
+        # preprocessing:
+        preprocess_edfs(subject=subject, task=task, edfs=edfs, output_dir=os.path.join(output_dir,task,subject))
+        
+def preprocess_edfs(subject, task, edfs, output_dir):
+    """import_all_data loops across the aliases of the sessions and converts the respective edf files, adds them to the self.ho's hdf5 file. """
+    
+    # folder hierarchy:
+    try:
+        os.makedirs(os.path.join(output_dir, 'raw'))
+    except OSError:
+        pass
+    
+    # hdf5 filename:
+    hdf5_filename = os.path.join(output_dir, '{}_{}.hdf5'.format(subject, task))
+    try:
+        os.remove(hdf5_filename)
+    except OSError:
+        pass
+    
+    # initialize hdf5 HDFEyeOperator:
+    ho = hedfpy.HDFEyeOperator(hdf5_filename)
+    
+    # variables:
+    session_nrs = [r.split('ses-')[1][0:2] for r in edfs]
+    run_nrs = [r.split('run-')[1][0:1] for r in edfs]
+    aliases = []
+    for i in range(len(session_nrs)):
+        aliases.append('{}_{}_{}'.format(task, session_nrs[i], run_nrs[i]))
+    
+    # preprocessing:
+    for (edf_file, alias,) in zip(edfs, aliases):
+        os.system('cp "' + edf_file + '" "' + os.path.join(output_dir, 'raw', alias + '.edf"'))
+    for alias in aliases:
+        ho.add_edf_file(os.path.join(output_dir, 'raw', alias + '.edf'))
+        ho.edf_message_data_to_hdf(alias=alias)
+        ho.edf_gaze_data_to_hdf(alias=alias)
+
+def main():
+    preprocess_subjects(subjects)
+
+if __name__ == '__main__':
+    main()
